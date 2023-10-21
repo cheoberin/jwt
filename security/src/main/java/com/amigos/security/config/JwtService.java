829368f8028ce2715cce9efb09c8a42741e8bb6b
Airton Ribeiro Manfrini Junior <airton.manfrini@hotmail.com>
2023-10-21 08:26:51 -0300

    Second Commit

---
+++ security/src/main/java/com/amigos/security/config/JwtService.java
@@ -0,0 +1,12 @@
+package com.amigos.security.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+
+    public String extractUserEmail(String token) {
+        return null;
+    }
+}
