f674c987115c9c70d7c3bf38d264fe2e95b02f94
Airton Ribeiro Manfrini Junior <airton.manfrini@hotmail.com>
2023-10-21 13:54:10 -0300

    add: JWT service.

--- security/src/main/java/com/amigos/security/config/JwtAuthenticationFilter.java
+++ security/src/main/java/com/amigos/security/config/JwtAuthenticationFilter.java
@@ -26,16 +26,18 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
+
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
         final String userEmail;
+
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
 
         jwt = authHeader.substring(7);
-        userEmail = jwtService.extractUserEmail(jwt);
+        userEmail = jwtService.extractUsername(jwt);
 
     }
 }
